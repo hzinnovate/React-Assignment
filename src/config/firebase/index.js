f5fd48c70c9d39063b5c19a7b9a087a91176d813
Syed Hasnain Zaidi <39019111+syedzaidi95@users.noreply.github.com>
2019-04-30 20:11:42 +0500

    Update index.js

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -2,11 +2,6 @@ import firebase from 'firebase/app';
 // import 'firebase/database';
 
 var config = {
-    apiKey: "AIzaSyDcE22D9cctjzVTakjC1DU1CcSlNEpfbJg",
-    authDomain: "learn-react-7f0a9.firebaseapp.com",
-    databaseURL: "https://learn-react-7f0a9.firebaseio.com",
-    projectId: "learn-react-7f0a9",
-    storageBucket: "learn-react-7f0a9.appspot.com",
-    messagingSenderId: "225730043440"
+   
   };
 export default firebase.initializeApp(config)
